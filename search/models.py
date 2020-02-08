6489f6133d8976a7ecae69f245b507b0f433a79a
Alex Gardell <agardell89@gmail.com>
2020-02-08 10:19:26 -0500

    add wellness professional model

--- search/models.py
+++ search/models.py
@@ -2,3 +2,27 @@ from django.db import models
 
 
 # Create your models here.
+class WellnessProfessionals(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    address_1 = models.CharField(max_length=255)
+    address_2 = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=75)
+    state = models.CharField(max_length=2)
+    zip = models.CharField(max_length=5)
+    credentials = models.TextField(default="", null=True, blank=True)
+    specialties = models.CharField(max_length=255, null=True, blank=True)
+    date_joined = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = '"wellness_prof"'
+
+    def getFullName(self):
+        if (self.title is not None and self.title != ""):
+            return self.title + " " + self.first_name + " " + self.last_name
+        else:
+            return self.first_name + " " + self.last_name        
+
+    def __str__(self):
+        return self.getFullName()
